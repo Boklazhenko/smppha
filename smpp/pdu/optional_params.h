559ef9b80011f097c5d8c39421d08b7e02e9d590
boklazhenko <aboklazhenko@stream.ru>
2020-11-16 14:19:07 +0500

    some optional params changes

--- smpp/pdu/optional_params.h
+++ smpp/pdu/optional_params.h
@@ -52,7 +52,7 @@ struct unknown_optional_param : i_optional_param {
   octet_string<std::numeric_limits<uint16_t>::max()> value;
 };
 
-template<opt_par_tag tag, typename t>
+template<uint16_t tag, typename t>
 struct optional_param : i_optional_param {
 
   using underlying_type = t;
@@ -83,107 +83,111 @@ struct optional_param : i_optional_param {
     return std::to_string(value);
   }
 
-  inline static const uint16_t tag_ = to_integral(tag);
+  inline static const uint16_t tag_ = tag;
   underlying_type value;
 };
 
-struct dest_addr_subunit : optional_param<opt_par_tag::dest_addr_subunit, uint8_t> {};
+template<opt_par_tag tag, typename t>
+struct optional_param_helper : optional_param<to_integral(tag), t> {};
 
-struct dest_network_type : optional_param<opt_par_tag::dest_network_type, uint8_t> {};
+struct dest_addr_subunit : optional_param_helper<opt_par_tag::dest_addr_subunit, uint8_t> {};
 
-struct source_network_type : optional_param<opt_par_tag::source_network_type, uint8_t> {};
+struct dest_network_type : optional_param_helper<opt_par_tag::dest_network_type, uint8_t> {};
 
-struct dest_bearer_type : optional_param<opt_par_tag::dest_bearer_type, uint8_t> {};
+struct source_network_type : optional_param_helper<opt_par_tag::source_network_type, uint8_t> {};
 
-struct source_bearer_type : optional_param<opt_par_tag::source_bearer_type, uint8_t> {};
+struct dest_bearer_type : optional_param_helper<opt_par_tag::dest_bearer_type, uint8_t> {};
 
-struct dest_telematics_id : optional_param<opt_par_tag::dest_telematics_id, uint16_t> {};
+struct source_bearer_type : optional_param_helper<opt_par_tag::source_bearer_type, uint8_t> {};
 
-struct source_addr_subunit : optional_param<opt_par_tag::source_addr_subunit, uint8_t> {};
+struct dest_telematics_id : optional_param_helper<opt_par_tag::dest_telematics_id, uint16_t> {};
 
-struct source_telematics_id : optional_param<opt_par_tag::source_telematics_id, uint8_t> {};
+struct source_addr_subunit : optional_param_helper<opt_par_tag::source_addr_subunit, uint8_t> {};
 
-struct qos_time_to_live : optional_param<opt_par_tag::qos_time_to_live, uint32_t> {};
+struct source_telematics_id : optional_param_helper<opt_par_tag::source_telematics_id, uint8_t> {};
 
-struct payload_type : optional_param<opt_par_tag::payload_type, uint8_t> {};
+struct qos_time_to_live : optional_param_helper<opt_par_tag::qos_time_to_live, uint32_t> {};
 
-struct additional_status_info_text : optional_param<opt_par_tag::additional_status_info_text, c_octet_string < 256>> {
+struct payload_type : optional_param_helper<opt_par_tag::payload_type, uint8_t> {};
+
+struct additional_status_info_text : optional_param_helper<opt_par_tag::additional_status_info_text,
+                                                           c_octet_string < 256>> {
 };
 
-struct receipted_message_id : optional_param<opt_par_tag::receipted_message_id, c_octet_string < 65>> {
+struct receipted_message_id : optional_param_helper<opt_par_tag::receipted_message_id, c_octet_string < 65>> {
 };
 
-struct ms_msg_wait_facilities : optional_param<opt_par_tag::ms_msg_wait_facilities, uint8_t> {};
+struct ms_msg_wait_facilities : optional_param_helper<opt_par_tag::ms_msg_wait_facilities, uint8_t> {};
 
-struct privacy_indicator : optional_param<opt_par_tag::privacy_indicator, uint8_t> {};
+struct privacy_indicator : optional_param_helper<opt_par_tag::privacy_indicator, uint8_t> {};
 
-struct source_subaddress : optional_param<opt_par_tag::source_subaddress, octet_string < 23>> {
+struct source_subaddress : optional_param_helper<opt_par_tag::source_subaddress, octet_string < 23>> {
 };
 
-struct dest_subaddress : optional_param<opt_par_tag::dest_subaddress, octet_string < 23>> {
+struct dest_subaddress : optional_param_helper<opt_par_tag::dest_subaddress, octet_string < 23>> {
 };
 
-struct user_message_reference : optional_param<opt_par_tag::user_message_reference, uint16_t> {};
+struct user_message_reference : optional_param_helper<opt_par_tag::user_message_reference, uint16_t> {};
 
-struct user_response_code : optional_param<opt_par_tag::user_response_code, uint8_t> {};
+struct user_response_code : optional_param_helper<opt_par_tag::user_response_code, uint8_t> {};
 
-struct source_port : optional_param<opt_par_tag::source_port, uint16_t> {};
+struct source_port : optional_param_helper<opt_par_tag::source_port, uint16_t> {};
 
-struct language_indicator : optional_param<opt_par_tag::language_indicator, uint8_t> {};
+struct language_indicator : optional_param_helper<opt_par_tag::language_indicator, uint8_t> {};
 
-struct destination_port : optional_param<opt_par_tag::destination_port, uint16_t> {};
+struct destination_port : optional_param_helper<opt_par_tag::destination_port, uint16_t> {};
 
-struct sar_msg_ref_num : optional_param<opt_par_tag::sar_msg_ref_num, uint16_t> {};
+struct sar_msg_ref_num : optional_param_helper<opt_par_tag::sar_msg_ref_num, uint16_t> {};
 
-struct sar_total_segments : optional_param<opt_par_tag::sar_total_segments, uint8_t> {};
+struct sar_total_segments : optional_param_helper<opt_par_tag::sar_total_segments, uint8_t> {};
 
-struct sar_segment_seqnum : optional_param<opt_par_tag::sar_segment_seqnum, uint8_t> {};
+struct sar_segment_seqnum : optional_param_helper<opt_par_tag::sar_segment_seqnum, uint8_t> {};
 
-struct sc_interface_version : optional_param<opt_par_tag::sc_interface_version, uint8_t> {};
+struct sc_interface_version : optional_param_helper<opt_par_tag::sc_interface_version, uint8_t> {};
 
-struct display_time : optional_param<opt_par_tag::display_time, uint8_t> {};
+struct display_time : optional_param_helper<opt_par_tag::display_time, uint8_t> {};
 
-struct ms_validity : optional_param<opt_par_tag::ms_validity, uint8_t> {};
+struct ms_validity : optional_param_helper<opt_par_tag::ms_validity, uint8_t> {};
 
-struct dpf_result : optional_param<opt_par_tag::dpf_result, uint8_t> {};
+struct dpf_result : optional_param_helper<opt_par_tag::dpf_result, uint8_t> {};
 
-struct set_dpf : optional_param<opt_par_tag::set_dpf, uint8_t> {};
+struct set_dpf : optional_param_helper<opt_par_tag::set_dpf, uint8_t> {};
 
-struct ms_availability_status : optional_param<opt_par_tag::ms_availability_status, uint8_t> {};
+struct ms_availability_status : optional_param_helper<opt_par_tag::ms_availability_status, uint8_t> {};
 
-struct network_error_code : optional_param<opt_par_tag::network_error_code, octet_string < 3>> {
+struct network_error_code : optional_param_helper<opt_par_tag::network_error_code, octet_string < 3>> {
 };
 
-struct message_payload : optional_param<opt_par_tag::message_payload,
-                                        octet_string < std::numeric_limits<uint16_t>::max()>> {
+struct message_payload : optional_param_helper<opt_par_tag::message_payload,
+                                               octet_string < std::numeric_limits<uint16_t>::max()>> {
 };
 
-struct delivery_failure_reason : optional_param<opt_par_tag::delivery_failure_reason, uint8_t> {};
+struct delivery_failure_reason : optional_param_helper<opt_par_tag::delivery_failure_reason, uint8_t> {};
 
-struct more_messages_to_send : optional_param<opt_par_tag::more_messages_to_send, uint8_t> {};
+struct more_messages_to_send : optional_param_helper<opt_par_tag::more_messages_to_send, uint8_t> {};
 
-struct message_state : optional_param<opt_par_tag::message_state, uint8_t> {};
+struct message_state : optional_param_helper<opt_par_tag::message_state, uint8_t> {};
 
-struct callback_num : optional_param<opt_par_tag::callback_num, octet_string < 19>> {
+struct callback_num : optional_param_helper<opt_par_tag::callback_num, octet_string < 19>> {
 };
 
-struct callback_num_pres_ind : optional_param<opt_par_tag::callback_num_pres_ind, uint8_t> {};
+struct callback_num_pres_ind : optional_param_helper<opt_par_tag::callback_num_pres_ind, uint8_t> {};
 
-struct callback_num_atag : optional_param<opt_par_tag::callback_num_atag, octet_string < 65>> {
+struct callback_num_atag : optional_param_helper<opt_par_tag::callback_num_atag, octet_string < 65>> {
 };
 
-struct number_of_messages : optional_param<opt_par_tag::number_of_messages, uint8_t> {};
+struct number_of_messages : optional_param_helper<opt_par_tag::number_of_messages, uint8_t> {};
 
-struct sms_signal : optional_param<opt_par_tag::sms_signal, uint16_t> {};
+struct sms_signal : optional_param_helper<opt_par_tag::sms_signal, uint16_t> {};
 
-struct alert_on_message_delivery : optional_param<opt_par_tag::alert_on_message_delivery, octet_string < 0>> {
+struct alert_on_message_delivery : optional_param_helper<opt_par_tag::alert_on_message_delivery, octet_string < 0>> {
 };
 
-struct its_reply_type : optional_param<opt_par_tag::its_reply_type, uint8_t> {};
+struct its_reply_type : optional_param_helper<opt_par_tag::its_reply_type, uint8_t> {};
 
-struct ussd_service_op : optional_param<opt_par_tag::ussd_service_op, uint8_t> {};
+struct ussd_service_op : optional_param_helper<opt_par_tag::ussd_service_op, uint8_t> {};
 
-struct its_session_info : optional_param<opt_par_tag::its_session_info, uint16_t> {};
+struct its_session_info : optional_param_helper<opt_par_tag::its_session_info, uint16_t> {};
 
 inline std::shared_ptr<i_optional_param> create_opt_param_by_tag(opt_par_tag tag) {
   using namespace std;
